fb2a08ff3101c87cb4627336e71d637dcf98828c
ESHA KUMARI <eshakumarijsr2002@gmail.com>
2024-07-04 21:24:27 +0530

    update

--- project/src/app/components/header/header.component.ts
+++ project/src/app/components/header/header.component.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 // const logo = require("../../../assets/logo.jpg").default as string;
 // import logo from '../../../assets/logo.jpg';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgbNavModule,NgbDropdownModule,NgbCarouselModule],
+  imports: [NgbNavModule,NgbDropdownModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   active = 0;
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   // logo1=logo;
- constructor(){
+ //constructor(){
     // this.logo="../../assets/logo.jpg";
-  }
+  //}
 }
 
