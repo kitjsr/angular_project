4b1ff766a3406619b71158a3ba922d62cb67f561
kunalkr4u <kunalkumar1987@gmail.com>
2024-07-04 19:29:08 +0530

    notice and product component

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { NoticeComponent } from './components/notice/notice.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent, NoticeComponent,ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- project/src/app/components/header/header.component.ts
+++ project/src/app/components/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-
+// const logo = require("../../../assets/logo.jpg").default as string;
+// import logo from '../../../assets/logo.jpg';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -13,5 +14,9 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 export class HeaderComponent {
   active = 0;
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  // logo1=logo;
+ constructor(){
+    // this.logo="../../assets/logo.jpg";
+  }
 }
 
