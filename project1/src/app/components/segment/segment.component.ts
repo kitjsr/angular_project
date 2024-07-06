52937e558a4a123bc51d1cf0ec5879374822f3c5
kitjsr <91665868+kitjsr@users.noreply.github.com>
2024-07-06 19:37:48 +0530

    update

--- project1/src/app/components/segment/segment.component.ts
+++ project1/src/app/components/segment/segment.component.ts
@@ -8,21 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './segment.component.css'
 })
 export class SegmentComponent {
-  // segments=[
-  //   {
-  //     box:1,
-  //     title:'WEDDING DECOR & TENTING',
-  //     description:'We specialize in Wedding Decor Design We at RTH believe in the beauty of our client dreams and our work is a leap of imagination which brings happiness and joy to them. Every bit of wedding design is the cumulative effect of a large infrastructure.',
-  //   },
-  //   {
-  //     box:2,
-  //     title:'DESTINATION WEDDINGS',
-  //     description:'Be it in India or overseas, RTH will take complete control of the event including assistance in selection of destination & Venue, decor, F&B, entertainment etc. Our years of experience in marriage logistics helps us plan and execute destination weddings without a flaw.',
-  //   },
-  //   {
-  //     box:3,
-  //     title:'CORPORATE EVENTS',
-  //     description:'RTH specializes in organizing customized Corporate Events, Award Functions, Musical Nights, Exhibitions & Stall Designing on pan India basis.',
-  //   },
-  // ];
+  segments=[
+    {
+      box:1,
+      title:'WEDDING DECOR & TENTING',
+      description:'We specialize in Wedding Decor Design We at RTH believe in the beauty of our client dreams and our work is a leap of imagination which brings happiness and joy to them. Every bit of wedding design is the cumulative effect of a large infrastructure.',
+    },
+    {
+      box:2,
+      title:'DESTINATION WEDDINGS',
+      description:'Be it in India or overseas, RTH will take complete control of the event including assistance in selection of destination & Venue, decor, F&B, entertainment etc. Our years of experience in marriage logistics helps us plan and execute destination weddings without a flaw.',
+    },
+    {
+      box:3,
+      title:'CORPORATE EVENTS',
+      description:'RTH specializes in organizing customized Corporate Events, Award Functions, Musical Nights, Exhibitions & Stall Designing on pan India basis.',
+    },
+  ];
 }
